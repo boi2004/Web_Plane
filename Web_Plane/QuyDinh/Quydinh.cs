e28501ca92a06853900096ad1030d39cac75e093
boi2004 <alorota24@gmail.com>
2024-06-21 15:33:32 +0700

    Quy định của nhóm

---
+++ Web_Plane/QuyDinh/Quydinh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Plane.QuyDinh
+{
+    public class Quydinh
+    {
+        /*
+         
+        Quy định đặt tên:
+            -Tên tiếng việt vd: ChucnangController......
+            -Bỏ view vào thư mục cho đúng dễ tìm kiếm
+            -Layout cũng bỏ vào thư mục cho dễ tìm kiếm
+            -Layout chung nằm trong Views/Shared/Layout và ViewStars là nơi bắt đầu view
+            
+         Th2:Git
+            -Để Lưu code về nên lên github check có ai tải lên chưa
+                1/ Nếu có người tải lên mình phải tải về rồi mới làm phần mình tránh trường hợp xung đột.
+                2/ Nếu xong thì vào group nhắn là đã đẩy code lên
+                3.Để lại lời nhắn lên git theo tên-số ,vd: Nguyên-01,Nguyên-02,Khôi-03......
+        Th3:Code
+            - Nên để comment lại trên từng dòng code để mọi người dễ biết lỗi và phân biệt
+            - Nói tóm tắt chức năng tại hàm để dễ fix
+         */
+    }
+}
