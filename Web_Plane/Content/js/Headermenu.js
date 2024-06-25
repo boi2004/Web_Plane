5c10c18a29e9b7f55474a172ea1c67457574ede4
Khoi <khoikhoi0911@gmail.com>
2024-06-25 22:28:53 +0700

    Header footer

---
+++ Web_Plane/Content/js/Headermenu.js
@@ -0,0 +1,8 @@
+﻿
+    const menuIcon = document.getElementById('menu-icons');
+    const navbarr = document.querySelector('.navbarr');
+
+    menuIcon.addEventListener('click', () => {
+        navbarr.classList.toggle('active');
+    });
+
